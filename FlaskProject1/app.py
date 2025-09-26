4ed942a6f6612141c6f0a2e914527dfd8d4c674e
TDTanev21 <TDTanev21@codingburgas.bg>
2025-09-26 11:31:40 +0300

    Make the basic structure of the project

--- FlaskProject1/app.py
+++ FlaskProject1/app.py
@@ -1,12 +1,11 @@
-from flask import Flask
-
+from flask import Flask,render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
 
+from .main import main_bp
+
+app.register_blueprint(main_bp)
 
 if __name__ == '__main__':
     app.run()
