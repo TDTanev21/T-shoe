31e1ccc019063fcd741bb1123dc9883f6630e779
TDTanev21 <TDTanev21@codingburgas.bg>
2025-09-28 22:54:13 +0300

    Make the register page work

---
+++ FlaskProject1/auth/accounts.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self):
+        self.accounts = []
+current_user = User()
+
+
--- FlaskProject1/auth/routes.py
+++ FlaskProject1/auth/routes.py
@@ -1,9 +1,9 @@
-from flask import render_template, redirect, url_for, flash, session, request
-from flask_login import login_user, logout_user, current_user
+from flask import render_template, redirect, url_for, flash, request
 from . import auth_bp
-from .forms import LoginForm, RegistrationForm
-from data.accounts import *
+from .forms import RegistrationForm
+from .accounts import current_user
 
+@auth_bp.route('/register', methods=['GET', 'POST'])
 @auth_bp.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
@@ -15,13 +15,15 @@ def register():
             if password != confirm:
                 flash('Passwords do not match', 'danger')
                 return render_template('auth/register.html', form=form)
-            if username in accounts:
+            if username in [u[0] for u in current_user.accounts]:
                 flash('Username already taken.', 'warning')
                 return render_template('auth/register.html', form=form)
-            accounts.append((username, password))
+            current_user.accounts.append((username, password))
+            print(current_user.accounts)
     except Exception as e:
         print(f"Registration Error: {e}")
         return redirect(url_for('errors.integrity_error'))
     return render_template('auth/register.html', form=form)
+
 
 
