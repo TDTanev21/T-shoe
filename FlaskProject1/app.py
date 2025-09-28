77f0412e761ec554c1a448bbb8b66ddeca1f4a62
TDTanev21 <TDTanev21@codingburgas.bg>
2025-09-29 00:33:33 +0300

    Make the fundamentals of the dashboard

--- FlaskProject1/app.py
+++ FlaskProject1/app.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from config import Config
+from auth.accounts import current_user
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'VERYSECRETKEY'
 
 
 from main import main_bp
 from auth import auth_bp
+from dashboard import dashboard_bp
 app.register_blueprint(main_bp)
 app.register_blueprint(auth_bp, url_prefix='/auth')
+app.register_blueprint(dashboard_bp, url_prefix='/dashboard')
 if __name__ == '__main__':
     app.run()
+
+@app.context_processor
+def inject_current_user():
+    return dict(current_user=current_user)
---
+++ FlaskProject1/dashboard/routes.py
@@ -0,0 +1,12 @@
+from flask import render_template, session, redirect, url_for, flash
+from auth.accounts import current_user
+from . import dashboard_bp
+
+@dashboard_bp.route('/dashboard')
+def dashboard():
+    if 'username' not in session:
+        flash('Моля, влезте в системата, за да видите дашборда.', 'warning')
+        return redirect(url_for('auth.login'))
+
+    current_user.username = session['username']
+    return render_template('dashboard/dashboard.html', current_user=current_user)
