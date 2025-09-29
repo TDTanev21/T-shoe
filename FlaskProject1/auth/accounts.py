fd8e84c2c2d4e027621cb1e7f356eb4df05548e2
TDTanev21 <TDTanev21@codingburgas.bg>
2025-09-29 05:26:56 +0300

    Make an admin role and fix some mistakes in the authentication

--- FlaskProject1/auth/accounts.py
+++ FlaskProject1/auth/accounts.py
@@ -1,6 +1,11 @@
+accounts = [
+    ('t', 't'),
+    ('admin', 'admin'),
+]
+
 class User:
     def __init__(self):
-        self.accounts = [('t','t')]
+        self.username = None
+        self.is_authenticated = False
+
 current_user = User()
-
-
